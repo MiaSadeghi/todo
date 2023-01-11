aae08dfa9ff198b3cced03920b0dae85a151d626
MiaSadeghi <kimia.saadeghi@gmail.com>
2023-01-11 16:52:37 -0700

    todoSlice added. Todo checkbox functionality added

--- src/components/tasks/ToDo.jsx
+++ src/components/tasks/ToDo.jsx
@@ -1,13 +1,21 @@
 import { Checkbox, FormControlLabel } from "@mui/material";
+import { useDispatch } from "react-redux";
+import { toggleCheck } from "../../redux/todoSlice";
 
 const ToDo = (props) => {
   const { text } = props;
-
-  const handleMarkAsDone = () => {};
+  const dispatch = useDispatch();
+  const handleCheckbox = () => {
+    dispatch(toggleCheck());
+    console.log(`${text} was modified`);
+  };
 
   return (
     <div className="task__item">
-      <FormControlLabel control={<Checkbox />} label={text} />{" "}
+      <FormControlLabel
+        control={<Checkbox onClick={handleCheckbox} />}
+        label={text}
+      />{" "}
     </div>
   );
 };
