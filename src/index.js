bc7767e31ec93d11900042b36e74a52b2e1eab02
MiaSadeghi <kimia.saadeghi@gmail.com>
2023-01-12 17:37:27 -0700

    MUI custom theme added. AppBar updated.

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import "./index.css";
 import App from "./App";
 import store from "./redux/store";
 import { Provider } from "react-redux";
-
+import { ThemeProvider } from "@mui/material";
+import theme from "./theme";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </Provider>,
 );
 
---
+++ src/theme.js
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: "#9061e2",
+      main: "#5e35b1",
+      dark: "#27057f",
+    },
+    info: {
+      main: "#1F2022",
+    },
+  },
+});
+
+export default theme;
