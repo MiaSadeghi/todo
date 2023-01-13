c433022ced868b2a917729978d3292fa19a6a783
MiaSadeghi <kimia.saadeghi@gmail.com>
2023-01-12 18:06:35 -0700

    navbar buttons updated to MUI

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,29 +1,44 @@
 import { useDispatch } from "react-redux";
 import { showAddTaskModal, toggleSideMenu } from "../../redux/layoutSlice";
-import { AppBar } from "@mui/material";
+import { AppBar, IconButton, Toolbar } from "@mui/material";
+// import { MenuIcon } from "@mui/icons-material/";
 import Search from "./Search";
 
 const Header = () => {
   const dispatch = useDispatch();
   return (
-    <AppBar position="sticky" color="primary" style={{ backgroundColor: "" }}>
-      <div className="header__controls-left">
-        <button onClick={() => dispatch(toggleSideMenu())}>
-          <i className="fa-solid fa-bars" />
-        </button>
-        <button>
-          <i className="fa-solid fa-house" />
-        </button>
-        <Search />
-      </div>
-      <div className="header__controls-right">
-        <button onClick={() => dispatch(showAddTaskModal())}>
-          <i className="fa-solid fa-plus" />
-        </button>
-        <button>
-          <i className="fa-regular fa-user" />
-        </button>
-      </div>
+    <AppBar position="sticky" color="primary">
+      <Toolbar>
+        <div className="header__controls-left">
+          <IconButton
+            onClick={() => dispatch(toggleSideMenu())}
+            color="inherit"
+            aria-label="sidemenu"
+            size="small"
+            edge="start"
+          >
+            {/* <MenuIcon color="inherit" /> */}
+            <i className="fa-solid fa-bars" />
+          </IconButton>
+          <IconButton color="inherit" aria-label="home" size="small">
+            <i className="fa-solid fa-house" />
+          </IconButton>
+          <Search />
+        </div>
+        <div className="header__controls-right">
+          <IconButton
+            onClick={() => dispatch(showAddTaskModal())}
+            color="inherit"
+            aria-label="add task"
+            size="small"
+          >
+            <i className="fa-solid fa-plus" />
+          </IconButton>
+          <IconButton color="inherit" aria-label="login" size="small">
+            <i className="fa-regular fa-user" />
+          </IconButton>
+        </div>
+      </Toolbar>
     </AppBar>
   );
 };
