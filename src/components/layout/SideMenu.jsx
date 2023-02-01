6f195f4b369f5f753e8e2661b9a44484e90fd538
MiaSadeghi <kimia.saadeghi@gmail.com>
2023-01-31 20:46:10 -0700

    side menu filters added

--- src/components/layout/SideMenu.jsx
+++ src/components/layout/SideMenu.jsx
@@ -11,15 +11,41 @@ import {
   ListItemText,
   ListItemIcon,
   Toolbar,
+  ListSubheader,
+  Collapse,
+  Box,
 } from "@mui/material";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
+import Inbox from "@mui/icons-material/Inbox";
+import Today from "@mui/icons-material/Today";
+import Upcoming from "@mui/icons-material/CalendarMonth";
+import Filter from "@mui/icons-material/Tune";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+import Add from "@mui/icons-material/Add";
+import Circle from "@mui/icons-material/Circle";
+import Person from "@mui/icons-material/Person";
 
 const drawerWidth = 240;
+
 const SideMenu = () => {
   const [projectNames, setProjectNames] = useState([]);
-
+  const [favoritesIsOpen, setFavoritesIsOpen] = useState(true);
+  const [projectsIsOpen, setProjectsIsOpen] = useState(true);
   const sideMenuIsOpen = useSelector((state) => state.layout.sideMenuOpen);
+
+  const mainFilters = [
+    { title: "Today", icon: <Today color="success" /> },
+    { title: "Upcoming", icon: <Upcoming color="primary" /> },
+    { title: "Inbox", icon: <Inbox color="secondary" /> },
+    { title: "Filters & Labels", icon: <Filter color="warning" /> },
+  ];
+
+  const toggleFavorites = () => {
+    setFavoritesIsOpen((prevState) => !prevState);
+  };
+  const toggleProjects = () => {
+    setProjectsIsOpen((prevState) => !prevState);
+  };
 
   useEffect(() => {
     const query = new Parse.Query(Project);
@@ -44,45 +70,82 @@ const SideMenu = () => {
       open={sideMenuIsOpen}
     >
       <Toolbar />
-
-      <List>
-        {projectNames.map((project) => (
-          <ListItem key={project.id} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                <InboxIcon />
-              </ListItemIcon>
-              <ListItemText primary={project.attributes.name} />
-            </ListItemButton>
-          </ListItem>
-        ))}
+      <List sx={{ mb: 3 }} dense>
+        {mainFilters.map((item) => {
+          return (
+            <ListItem key={item.title} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>{item.icon}</ListItemIcon>
+                <ListItemText primary={item.title} />
+              </ListItemButton>
+            </ListItem>
+          );
+        })}
       </List>
-      <Divider />
-      <List>
-        {["All mail", "Trash", "Spam"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
+
+      <List
+        dense
+        subheader={
+          <ListSubheader
+            component="div"
+            id="favorites"
+            sx={{ display: "flex", justifyContent: "space-between" }}
+          >
+            <ListItemText secondary="Favorites" />
+            <ListItemIcon onClick={toggleFavorites}>
+              {favoritesIsOpen ? <ExpandLess /> : <ExpandMore />}
+            </ListItemIcon>
+          </ListSubheader>
+        }
+        sx={{ mb: 3 }}
+      >
+        <Collapse in={favoritesIsOpen} timeout="auto" unmountOnExit>
+          {projectNames.map((project) => (
+            <ListItem key={project.id} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <Circle fontSize="8px " />
+                </ListItemIcon>
+                <ListItemText primary={project.attributes.name} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </Collapse>
+      </List>
+
+      <List
+        dense
+        subheader={
+          <ListSubheader
+            component="div"
+            id="projects"
+            sx={{ display: "flex", justifyContent: "space-between" }}
+          >
+            <ListItemText secondary="Projects" />
+
+            <ListItemIcon>
+              <Add />
+            </ListItemIcon>
+            <ListItemIcon onClick={toggleProjects}>
+              {projectsIsOpen ? <ExpandLess /> : <ExpandMore />}
+            </ListItemIcon>
+          </ListSubheader>
+        }
+      >
+        <Collapse in={projectsIsOpen} timeout="auto" unmountOnExit>
+          {projectNames.map((project) => (
+            <ListItem key={project.id} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <Circle fontSize="8px " />
+                </ListItemIcon>
+                <ListItemText primary={project.attributes.name} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </Collapse>
       </List>
     </Drawer>
-
-    // <Drawer anchor="left" variant="persistent" className="sidemenu">
-    //   <div>
-    //     <div className="sidemenu-title">Projects</div>
-    //     <ul>
-    //       <li>Errands</li>
-    //       <li>Personal</li>
-    //       <li>Shopping List</li>
-    //       {mappedProjects}
-    //     </ul>
-    //   </div>
-    // </Drawer>
   );
 };
 
