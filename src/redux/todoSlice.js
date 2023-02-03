d8d9d344e87758817455e9c32ff0bf4eece9ae3c
MiaSadeghi <kimia.saadeghi@gmail.com>
2023-02-02 17:10:16 -0700

    todo slice updated in redux store

--- src/redux/todoSlice.js
+++ src/redux/todoSlice.js
@@ -3,15 +3,34 @@ import { createSlice } from "@reduxjs/toolkit";
 const todoSlice = createSlice({
   name: "todo",
   initialState: {
-    isChecked: false,
+    task: {
+      isChecked: false,
+      title: "",
+      dueDate: "",
+      project: "",
+      description: "",
+      priority: "",
+    },
+    project: {
+      title: "",
+      color: "",
+      isFavorite: false,
+      view: "list",
+    },
+
+    projects: [],
   },
   reducers: {
     toggleCheck: (state) => {
-      state.isChecked = !state.isChecked;
+      state.task.isChecked = !state.task.isChecked;
+    },
+
+    addProject: (state, action) => {
+      state.projects.push(action.payload);
     },
   },
 });
 
-export const { toggleCheck } = todoSlice.actions;
+export const { toggleCheck, addProject } = todoSlice.actions;
 
 export default todoSlice.reducer;
